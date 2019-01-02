783ab7badba7b39b6c709125a4e2da7e01e3f38b
Cori Beemish <cbeemish@gmail.com>
2019-01-02 13:07:18 -0500

    Create endUp.java

---
+++ CodingBat/src/Warmup-1/endUp.java
@@ -0,0 +1,19 @@
+// Completed on January 2nd, 2018
+
+// Given a string, return a new string where the last 3 chars are now in upper case. If the string has less than 3 chars, uppercase whatever is there. Note that str.toUpperCase() returns the uppercase version of a string.
+
+// endUp("Hello") → "HeLLO"
+// endUp("hi there") → "hi thERE"
+// endUp("hi") → "HI"
+
+public String endUp(String str) {
+  if (str.length() <= 3){
+    return (str.toUpperCase());
+  }
+  
+  int startUpperCase = str.length() - 3;
+  
+  String upperCase = str.substring(startUpperCase, str.length()).toUpperCase();
+  
+  return (str.substring(0,startUpperCase) + upperCase);
+}
