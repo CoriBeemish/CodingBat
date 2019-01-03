f477237a9a4a8b0d221ac25547d7e3d261a7d65a
Cori Beemish <cbeemish@gmail.com>
2019-01-03 12:09:52 -0500

    Create countXX.java

---
+++ Warmup-2/countXX.java
@@ -0,0 +1,30 @@
+// Completed on January 3rd, 2018
+
+// Count the number of "xx" in the given string. We'll say that overlapping is allowed, so "xxx" contains 2 "xx".
+
+// countXX("abcxx") → 1
+// countXX("xxx") → 2
+// countXX("xxxx") → 3
+
+public class countXX {
+    public static void main(String[] args) {
+        countXX test = new countXX();
+
+        System.out.println(test.countXX("abcxx"));
+        System.out.println(test.countXX("xxx"));
+        System.out.println(test.countXX("xxxx"));
+    }
+
+    int countXX(String str) {
+        int count = 0;
+
+        for(int i = 0; i < str.length() - 1; i++){
+            if(str.substring(i, i+2).equals("xx")){
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+}
