367f4e2cd5c65c53e972d7cd99bd1bf054461cf9
Cori Beemish <cbeemish@gmail.com>
2019-01-03 15:23:06 -0500

    Create nonStart.java

---
+++ String-1/nonStart.java
@@ -0,0 +1,24 @@
+// Completed on January 3rd, 2018
+
+// Given 2 strings, return their concatenation, except omit the first char of each. The strings will be at least length 1.
+
+// nonStart("Hello", "There") → "ellohere"
+// nonStart("java", "code") → "avaode"
+// nonStart("shotl", "java") → "hotlava"
+
+public class nonStart {
+
+    public static void main(String[] args) {
+        nonStart test = new nonStart();
+
+        System.out.println(test.nonStart("Hello", "There"));
+        System.out.println(test.nonStart("java", "code"));
+        System.out.println(test.nonStart("shotl", "java"));
+    }
+
+    public String nonStart(String a, String b) {
+        String start1 = a.substring(1);
+        String start2 =b.substring(1);
+        return start1 + start2;
+    }
+}
