88b266b161822ddade13c15a84667fd16de2e4f5
Cori Beemish <cbeemish@gmail.com>
2019-01-03 14:26:47 -0500

    Create array667.java

---
+++ Warmup-2/array667.java
@@ -0,0 +1,33 @@
+// Completed on January 3rd, 2018
+
+// Given an array of ints, return the number of times that two 6's are next to each other in the array. Also count instances where the second "6" is actually a 7.
+
+// array667([6, 6, 2]) → 1
+// array667([6, 6, 2, 6]) → 1
+// array667([6, 7, 2, 6]) → 1
+
+public class array667 {
+
+    public static void main(String[] args) {
+        array667 test = new array667();
+
+        System.out.println(test.array667(new int[]{6, 6, 2}));
+        System.out.println(test.array667(new int[]{6, 6, 2, 6}));
+        System.out.println(test.array667(new int[]{6, 7, 2, 6}));
+    }
+
+    public int array667(int[] nums) {
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++ ){
+            if(nums.length > i+1 && nums[i] == 6 && nums[i+1] == 6){
+                count++;
+            }else if(nums.length > i+1 && nums[i] == 6 && nums[i+1] == 7 ){
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+}
