917e5bfded7b5f681f79ba8c08e9ebd8767f69f7
Cori Beemish <cbeemish@gmail.com>
2019-01-03 13:38:51 -0500

    Create altPairs.java

---
+++ Warmup-2/altPairs.java
@@ -0,0 +1,33 @@
+// Completed on January 3rd, 2018
+
+// Given a string, return a string made of the chars at indexes 0,1, 4,5, 8,9 ... so "kittens" yields "kien".
+
+// altPairs("kitten") → "kien"
+// altPairs("Chocolate") → "Chole"
+// altPairs("CodingHorror") → "Congrr"
+
+public class altPairs {
+
+    public static void main(String[] args) {
+        altPairs test = new altPairs();
+
+        System.out.println(test.altPairs("kitten"));
+        System.out.println(test.altPairs("Chocolate"));
+        System.out.println(test.altPairs("CodingHorror"));
+    }
+
+    public String altPairs(String str) {
+        String result = "";
+
+        for (int i = 0; i < str.length(); i += 4) {
+            if (i < str.length()-1)
+                result += str.substring(i, i+2);
+            else
+                result += str.substring(i, i+1);
+        }
+
+        return result;
+
+    }
+
+}
