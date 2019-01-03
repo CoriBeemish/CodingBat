7b3d55998d0ff74a052bb332c81aa12f983fff20
Cori Beemish <cbeemish@gmail.com>
2019-01-03 13:01:18 -0500

    Create array123.java

---
+++ Warmup-2/array123.java
@@ -0,0 +1,28 @@
+// Completed on January 3rd, 2018
+
+// Given an array of ints, return true if the sequence of numbers 1, 2, 3 appears in the array somewhere.
+
+// array123([1, 1, 2, 3, 1]) → true
+// array123([1, 1, 2, 4, 1]) → false
+// array123([1, 1, 2, 1, 2, 3]) → true
+
+public class array123 {
+
+    public static void main(String[] args) {
+        array123 test = new array123();
+
+        System.out.println(test.array123(new int[]{1, 1, 2, 3, 1}));
+        System.out.println(test.array123(new int[]{1, 1, 2, 4, 1}));
+        System.out.println(test.array123(new int[]{1, 1, 2, 1, 2, 3}));
+    }
+
+    public boolean array123(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums.length > i + 2 && nums[i] == 1 && nums[i + 1] == 2 && nums[i + 2] == 3) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
