c98587700813b74b83c513c67e477b7645313adb
Cori Beemish <cbeemish@gmail.com>
2019-01-03 15:12:52 -0500

    Create withoutEnd.java

---
+++ String-1/withoutEnd.java
@@ -0,0 +1,22 @@
+// Completed on January 3rd, 2018
+
+// Given a string, return a version without the first and last char, so "Hello" yields "ell". The string length will be at least 2.
+
+// withoutEnd("Hello") → "ell"
+// withoutEnd("java") → "av"
+// withoutEnd("coding") → "odin"
+
+public class withoutEnd {
+
+    public static void main(String[] args) {
+        withoutEnd test = new withoutEnd();
+
+        System.out.println(test.withoutEnd("Hello"));
+        System.out.println(test.withoutEnd("java"));
+        System.out.println(test.withoutEnd("coding"));
+    }
+
+    public String withoutEnd(String str) {
+        return str.substring(1, str.length()-1);
+    }
+}
