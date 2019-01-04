1226a51a679650cd964e15aafe36b83a8e1d53f2
Cori Beemish <cbeemish@gmail.com>
2019-01-04 15:42:49 -0500

    Create middleThree.java

---
+++ String-1/middleThree.java
@@ -0,0 +1,27 @@
+// Completed on January 4th, 2018
+
+// Given a string of odd length, return the string length 3 from its middle, so "Candy" yields "and". The string length will be at least 3.
+
+// middleThree("Candy") → "and"
+// middleThree("and") → "and"
+// middleThree("solving") → "lvi"
+
+public class middleThree {
+
+    public static void main(String[] args) {
+        middleThree test = new middleThree();
+
+        System.out.println(test.middleThree("Candy"));
+        System.out.println(test.middleThree("and"));
+        System.out.println(test.middleThree("solving"));
+    }
+
+    public String middleThree(String str) {
+        if(str.length() <= 3){
+            return str;
+        }
+        int middle = str.length() / 2;
+        return str.substring(middle - 1, middle + 2);
+    }
+
+}
