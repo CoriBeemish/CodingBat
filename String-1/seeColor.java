144ba44b38f5aca4e543315a540880a2f63bc4ec
Cori Beemish <cbeemish@gmail.com>
2019-01-04 16:29:12 -0500

    Create seeColor.java

---
+++ String-1/seeColor.java
@@ -0,0 +1,31 @@
+// Completed on January 4th, 2018
+
+// Given a string, if the string begins with "red" or "blue" return that color string, otherwise return the empty string.
+
+// seeColor("redxx") → "red"
+// seeColor("xxred") → ""
+// seeColor("blueTimes") → "blue"
+
+public class seeColor {
+
+    public static void main(String[] args) {
+        seeColor test = new seeColor();
+
+        System.out.println(test.seeColor("redxx"));
+        System.out.println(test.seeColor("xxred"));
+        System.out.println(test.seeColor("blueTimes"));
+    }
+
+    public String seeColor(String str) {
+        if (str.startsWith("red")) {
+            return "red";
+        }
+        else if (str.startsWith("blue")) {
+            return "blue";
+        }
+        else {
+            return "";
+        }
+    }
+
+}
