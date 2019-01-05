e1346a7516a474aa795d2e95c9c1947c4b9e51e7
Cori Beemish <cbeemish@gmail.com>
2019-01-04 19:34:51 -0500

    Create without2.java

---
+++ String-1/without2.java
@@ -0,0 +1,27 @@
+// Completed on January 4th, 2018
+
+// Given a string, if a length 2 substring appears at both its beginning and end, return a string without the substring at the beginning, so "HelloHe" yields "lloHe". The substring may overlap with itself, so "Hi" yields "". Otherwise, return the original string unchanged.
+
+// without2("HelloHe") → "lloHe"
+// without2("HelloHi") → "HelloHi"
+// without2("Hi") → ""
+
+public class without2 {
+
+    public static void main(String[] args) {
+        without2 test = new without2();
+
+        System.out.println(test.without2("HelloHe"));
+        System.out.println(test.without2("HelloHi"));
+        System.out.println(test.without2("Hi"));
+        System.out.println(test.without2(""));
+    }
+
+    public String without2(String str) {
+        if (str.length() >= 2 && str.substring(0, 2).equals(str.substring(str.length() - 2))){
+            return str.substring(2, str.length());
+        }
+        return str;
+    }
+
+}
