213d817fcc270cae0f1fe16f3ef3eddb1aa180e8
Cori Beemish <cbeemish@gmail.com>
2019-01-04 19:54:09 -0500

    Create withoutX.java

---
+++ String-1/withoutX.java
@@ -0,0 +1,31 @@
+// Completed on January 4th, 2018
+
+// Given a string, if the first or last chars are 'x', return the string without those 'x' chars, and otherwise return the string unchanged.
+
+// withoutX("xHix") → "Hi"
+// withoutX("xHi") → "Hi"
+// withoutX("Hxix") → "Hxi"
+
+public class withoutX {
+
+    public static void main(String[] args) {
+        withoutX test = new withoutX();
+
+        System.out.println(test.withoutX("xHix"));
+        System.out.println(test.withoutX("xHi"));
+        System.out.println(test.withoutX("Hxix"));
+    }
+
+    public String withoutX(String str) {
+        if(str.length() > 0 && str.charAt(0) == 'x'){
+            str = str.substring(1);
+        }
+
+        if(str.length() > 0 && str.charAt(str.length() -1) == 'x'){
+            str = str.substring(0, str.length() - 1);
+        }
+
+        return str;
+    }
+
+}
