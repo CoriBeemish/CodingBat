085ad5d859aa12c3fb53f5efee44b46fd70f28a0
Cori Beemish <cbeemish@gmail.com>
2019-01-07 10:47:42 -0500

    Create old35.java

---
+++ Logic-1/old35.java
@@ -0,0 +1,32 @@
+// Completed on January 7th, 2018
+
+// Return true if the given non-negative number is a multiple of 3 or 5, but not both. Use the % "mod" operator -- see Introduction to Mod
+
+// old35(3) → true
+// old35(10) → true
+// old35(15) → false
+
+public class old35 {
+
+    public static void main(String[] args) {
+        old35 test = new old35();
+
+        System.out.println(test.old35(3));
+        System.out.println(test.old35(10));
+        System.out.println(test.old35(15));
+    }
+
+    public boolean old35(int n) {
+        if(n%3 == 0 && n%5 == 0){
+            return false;
+        }
+
+        else if(n%3 == 0 || n%5 == 0){
+            return true;
+        }
+
+        return false;
+        // return (n % 3 == 0) != (n % 5 == 0);
+    }
+
+}
