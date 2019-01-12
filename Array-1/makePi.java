b6b2bbeb9d7306df50cec40440d772137ff7c7f8
Cori Beemish <cbeemish@gmail.com>
2019-01-12 16:38:23 -0500

    Create makePi.java

---
+++ Array-1/makePi.java
@@ -0,0 +1,21 @@
+// Completed on January 12th, 2019
+
+// Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+
+// makePi() → [3, 1, 4]
+
+// This one is terribly easy.
+
+public class makePi {
+
+    public static void main(String[] args) {
+        makePi test = new makePi();
+
+        System.out.println(test.makePi(new int[] {3, 1, 4}));
+    }
+
+    public int[] makePi(int[] nums) {
+        return new int[] {3, 1, 4};
+    }
+
+}
