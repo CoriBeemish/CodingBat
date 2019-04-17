9c92eb9720ac804895531544e4caee60606ac654
Cori Beemish <cbeemish@gmail.com>
2019-04-16 22:24:11 -0400

    Update getSandwich.java

--- Java/String-2/getSandwich.java
+++ Java/String-2/getSandwich.java
@@ -1,6 +1,6 @@
 // FILE: getSandwich.java
 
-// Completed on April 11th, 2019
+// Completed on April 16th, 2019
 
 // A sandwich is two pieces of bread with something in between. Return the string that is between the first and last appearance of "bread" in the given string, or return the empty string "" if there are not two pieces of bread.
 
@@ -11,7 +11,21 @@
 public class getSandwich {
 
     public static void main(String[] args) {
+        getSandwich test = new getSandwich();
 
+        System.out.println(">" + test.getSandwich("breadjambread") + "<");
+        System.out.println(">" + test.getSandwich("xxbreadjambreadyy") + "<");
+        System.out.println(">" + test.getSandwich("xxbreadyy") + "<");
     }
 
+    public String getSandwich(String str) {
+        int start = str.indexOf("bread");
+        int end = str.lastIndexOf("bread");
+
+        if(start != end){
+            return str.substring(start+5, end);
+        }
+
+        return "";
+    }
 }
